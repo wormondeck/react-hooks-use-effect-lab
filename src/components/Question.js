b220015772a1f4bf985c607f7a4a480432f8d47a
Ronney Arias <ronneyarias@gmail.com>
2024-02-29 16:45:50 -0600

    complete lab

--- src/components/Question.js
+++ src/components/Question.js
@@ -1,15 +1,34 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 
 function Question({ question, onAnswered }) {
   const [timeRemaining, setTimeRemaining] = useState(10);
 
   // add useEffect code
-
+  useEffect(() => {
+    if (timeRemaining === 0) {
+      setTimeRemaining(10)
+      onAnswered(false)
+    } 
+    const timerID = setTimeout(() => {
+      setTimeRemaining(prev => prev - 1);
+    }, 1000)
+    
+    return function cleanup() {
+      clearTimeout(timerID)
+    }
+    
+  }, [timeRemaining, onAnswered])
+  
+  
+  
+  
+  
+  
   function handleAnswer(isCorrect) {
     setTimeRemaining(10);
     onAnswered(isCorrect);
   }
-
+  
   const { id, prompt, answers, correctIndex } = question;
 
   return (
